bf1b891e032469453528169554e9cf7236c5b51b
ashraysripathi <42443206+ashraysripathi@users.noreply.github.com>
2019-12-02 23:34:38 +0530

    First Build

--- app.py
+++ app.py
@@ -1,50 +1,102 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, render_template, request
+import re
+from datetime import datetime
+
+# import our OCR function
+from ocr_core import ocr_core
+
+#  our extract data function
+def getrawdate(ocr_dump_text):
+    
+    ocr_dump_text=ocr_dump_text.upper()
+    extracted_date=""
+    try:                                                            
+        match = re.search(r'(\d{1,4}([.\-\\/])\d{1,2}([.\-\\/])\d{1,4})',ocr_dump_text)
+        extracted_date=match.group(1)
+    except:
+        extracted_date=ocr_dump_text    
+        try:
+            match = re.search(r'(\d{1,4}([.\-\\/])\D{1,3}([.\-\\/])\d{1,4})',ocr_dump_text)
+            extracted_date=match.group(1)
+        except:
+            extracted_date=ocr_dump_text  
+    extracted_date=extracted_date.replace("/","-")
+    extracted_date=extracted_date.replace("\\","-")
+    return extracted_date
+
+def getdate(extracted_date):       #Its hard to recognize DD/MM/YY or MM/DD/YY in some cases so it will assume dd/mm/yy unless values are outside expected range 
+    try:
+        my_date= datetime.strptime(extracted_date, "%d-%m-%Y")       
+    except:
+        
+        try:
+            my_date= datetime.strptime(extracted_date, "%d-%m-%y")       
+        except:
+            
+            try:
+                my_date= datetime.strptime(extracted_date, "%m-%d-%Y")       
+            except:
+                
+                try:
+                    my_date= datetime.strptime(extracted_date, "%m-%d-%y")       
+                except:
+                    
+                    try:
+                        my_date= datetime.strptime(extracted_date, "%d-%b-%Y")       
+                    except:
+                        
+                        try:
+                            my_date= datetime.strptime(extracted_date, "%d-%b-%y")       
+                        except:
+                            my_date="null"
+            
+    
+    return my_date
+
+# define a folder to store and later serve the images
+UPLOAD_FOLDER = '/static/uploads/'
+
+# allow files of a specific type
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+# function to check the file extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
+# route and function to handle the home page
 @app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+def home_page():
+    return render_template('index.html')
+
+# route and function to handle the upload page
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+        # check if there is a file in the request
+        if 'file' not in request.files:
+            return render_template('upload.html', msg='No file selected')
+        file = request.files['file']
+        # if no file is selected
+        if file.filename == '':
+            return render_template('upload.html', msg='No file selected')
+
+        if file and allowed_file(file.filename):
+
+            # call the OCR function on it
+            ocr_dump_text = ocr_core(file)
+            extracted_date=getrawdate(ocr_dump_text)
+            extracted_text=getdate(extracted_date).date()
+            # extract the text and display it
+            return render_template('upload.html',
+                                   msg='Successfully processed',
+                                   extracted_text=extracted_text,
+                                   img_src=UPLOAD_FOLDER + file.filename)
+    elif request.method == 'GET':
+        return render_template('upload.html')
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run()
